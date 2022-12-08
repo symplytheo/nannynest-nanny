da9c4f24e13c6ec4c0fa4cacc89e051c387da9b4
Theo Ojochegbe <symplytheo@gmail.com>
2022-12-08 19:36:42 +0100

    feat: implement dashboard interface

--- src/screen/dashboard/styles.js
+++ src/screen/dashboard/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 import { Colors } from '../../styles/colors';
 
 const styles = StyleSheet.create({
@@ -12,6 +12,64 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 16,
     borderBottomColor: Colors.grey + '33',
+  },
+  shadow: {
+    elevation: 6,
+    shadowOffset: { width: 2, height: 2 },
+    shadowColor: '#29101E',
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+  },
+  bold: {
+    fontWeight: Platform.OS === 'ios' ? '700' : '600',
+  },
+  card: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 24,
+    paddingHorizontal: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginVertical: 32,
+  },
+  earningBtn: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    backgroundColor: Colors.white + '44',
+    borderRadius: 24,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  sessionCard: {
+    backgroundColor: Colors.primary,
+    padding: 16,
+    borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  orderCard: {
+    borderRadius: 16,
+    height: 164,
+    width: '48%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.secondaryContainer,
+  },
+  orderCardText: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  orderCardBadge: {
+    marginLeft: 8,
+    backgroundColor: Colors.primary,
+    padding: 4,
+    borderRadius: 28,
+  },
+  orderSession: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
